202b951f63fb81d66fa0761679eb228feb30fcd9
= <avula.usedu1@gmail.com>
2026-03-09 18:12:22 -0500

    demo2 is done

--- demo2.js
+++ demo2.js
@@ -2,20 +2,34 @@ class Reservation extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        
+        isGoing:true,
+        numberOfGuests:2
       };
   
       this.handleInputChange = this.handleInputChange.bind(this);
     }
   
     handleInputChange(event) {
-      
+      const target = event.target;
+      const value = target.type === 'checkbox'? target.checked: target.value;
+      const name = target.name;
+      this.setState({
+        [name]:value
+      });
     }
   
     render() {
       return (
         <form>
-          
+          <label >
+              isgoing:
+              <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange}></input>
+          </label>
+          <br/>
+          <label>
+            Number of guests:
+            <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange}></input>
+          </label>
         </form>
       );
     }
