5d2696db8a401865a815bbd6169d470e77f47ece
= <avula.usedu1@gmail.com>
2026-03-09 18:25:28 -0500

    demo 3 and demo 4 are done

--- demo3.js
+++ demo3.js
@@ -1,11 +1,15 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
   class NameForm extends React.Component {
     constructor(props) {
       super(props);
-      
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.input = React.createRef();
     }
   
     handleSubmit(event) {
-      
+      alert("A name was submitted: "+ this.input.current.value);
+      event.preventDefault();
     }
   
     render() {
@@ -13,7 +17,7 @@
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text"  />
+            <input type="text"  ref={this.input} />
           </label>
           <input type="submit" value="Submit" />
         </form>
